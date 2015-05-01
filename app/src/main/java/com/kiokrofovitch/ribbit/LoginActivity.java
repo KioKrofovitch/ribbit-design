6c0e2856928e8975883f45d3be7d5eed7b5f6ca1
KioKrofovitch <kshuster@itriagehealth.com>
2015-05-01 16:09:53 -0600

    Adding in all assets, and beginning to design login page

--- app/src/main/java/com/kiokrofovitch/ribbit/LoginActivity.java
+++ app/src/main/java/com/kiokrofovitch/ribbit/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.kiokrofovitch.ribbit;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -17,88 +18,92 @@ import com.parse.ParseUser;
 
 public class LoginActivity extends Activity {
 
-	protected EditText mUsername;
-	protected EditText mPassword;
-	protected Button mLoginButton;
-	
-	protected TextView mSignUpTextView;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS); // <--- Must be called before setContentView or the app will crash!
-		
-		setContentView(R.layout.activity_login);
-		
-		mSignUpTextView = (TextView)findViewById(R.id.signUpText);
-		mSignUpTextView.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-				startActivity(intent);
-			}
-		});
-		
-		mUsername = (EditText) findViewById(R.id.usernameField);
-		mPassword = (EditText) findViewById(R.id.passwordField);
-		mLoginButton = (Button) findViewById(R.id.loginButton);
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected Button mLoginButton;
 
-		mLoginButton.setOnClickListener(new OnClickListener() {
+    protected TextView mSignUpTextView;
 
-			@Override
-			public void onClick(View v) {
-				// get input from user
-				String username = mUsername.getText().toString();
-				String password = mPassword.getText().toString();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-				// Trim whitespace
-				username = username.trim();
-				password = password.trim();
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS); // <--- Must be called before setContentView or the app will crash!
 
-				if ( username.isEmpty() || password.isEmpty() ) {
+        setContentView(R.layout.activity_login);
 
-					AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        ActionBar actionBar = getActionBar();
+        if(actionBar != null) {
+            actionBar.hide();
+        }
 
-					builder.setMessage(R.string.login_error_message)
-							.setTitle(R.string.login_error_title)
-							.setPositiveButton(android.R.string.ok, null);
+        mSignUpTextView = (TextView) findViewById(R.id.signUpText);
+        mSignUpTextView.setOnClickListener(new OnClickListener() {
 
-					AlertDialog dialog = builder.create();
-					dialog.show();
-				} else {
-					// login the user
-					setProgressBarIndeterminateVisibility(true);
-					ParseUser.logInInBackground(username, password, new LogInCallback() {
-						@Override
-						public void done(ParseUser user, ParseException e) {
-							setProgressBarIndeterminateVisibility(false);
-							if( e == null ){
-								// success!
-								Intent intent = new Intent(LoginActivity.this,
-										MainActivity.class);
-								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-								startActivity(intent);
-							}
-							else {
-								AlertDialog.Builder builder = new AlertDialog.Builder(
-										LoginActivity.this);
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
 
-								builder.setMessage(e.getMessage())
-										.setTitle(R.string.login_error_title)
-										.setPositiveButton(android.R.string.ok,
-												null);
+        mUsername = (EditText) findViewById(R.id.usernameField);
+        mPassword = (EditText) findViewById(R.id.passwordField);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
 
-								AlertDialog dialog = builder.create();
-								dialog.show();
-							}
-						}
-					});
-				}
-			}
-		});
-	}
-	
+        mLoginButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // get input from user
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                // Trim whitespace
+                username = username.trim();
+                password = password.trim();
+
+                if (username.isEmpty() || password.isEmpty()) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+
+                    builder.setMessage(R.string.login_error_message)
+                            .setTitle(R.string.login_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    // login the user
+                    setProgressBarIndeterminateVisibility(true);
+                    ParseUser.logInInBackground(username, password, new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            setProgressBarIndeterminateVisibility(false);
+                            if (e == null) {
+                                // success!
+                                Intent intent = new Intent(LoginActivity.this,
+                                        MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(
+                                        LoginActivity.this);
+
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.login_error_title)
+                                        .setPositiveButton(android.R.string.ok,
+                                                null);
+
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
 }
