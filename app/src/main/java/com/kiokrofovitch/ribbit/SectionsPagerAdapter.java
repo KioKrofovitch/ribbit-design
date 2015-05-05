0bdc74c075248ddda54043c82a82d43c45e73750
KioKrofovitch <kshuster@itriagehealth.com>
2015-05-04 22:12:34 -0600

    Customized Action Bar complete

--- app/src/main/java/com/kiokrofovitch/ribbit/SectionsPagerAdapter.java
+++ app/src/main/java/com/kiokrofovitch/ribbit/SectionsPagerAdapter.java
@@ -1,11 +1,11 @@
 package com.kiokrofovitch.ribbit;
 
-import java.util.Locale;
-
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
 
 /**
  * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -13,40 +13,51 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-	protected Context mContext;
-	
-	public SectionsPagerAdapter(Context context, FragmentManager fm) {
-		super(fm);
-		mContext = context;
-	}
+    protected Context mContext;
 
-	@Override
-	public Fragment getItem(int position) {
-		// getItem is called to instantiate the fragment for the given page.
-		switch( position ){
-			case 0:
-				return new InboxFragment();
-			case 1:
-				return new FriendsFragment();
-			default:
-				return null;
-		}
-	}
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
 
-	@Override
-	public int getCount() {
-		return 2;
-	}
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        switch (position) {
+            case 0:
+                return new InboxFragment();
+            case 1:
+                return new FriendsFragment();
+            default:
+                return null;
+        }
+    }
 
-	@Override
-	public CharSequence getPageTitle(int position) {
-		Locale l = Locale.getDefault();
-		switch (position) {
-		case 0:
-			return mContext.getString(R.string.title_section1).toUpperCase(l);
-		case 1:
-			return mContext.getString(R.string.title_section2).toUpperCase(l);
-		}
-		return null;
-	}
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.title_section2).toUpperCase(l);
+        }
+        return null;
+    }
+
+    public int getIcon(int position){
+        switch (position) {
+            case 0:
+                return R.drawable.ic_tab_inbox;
+            case 1:
+                return R.drawable.ic_tab_friends;
+            default:
+                return R.drawable.ic_tab_inbox;
+        }
+    }
 }
