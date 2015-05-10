614a85612a8efaf30cca8cef4a47cd258f1eb601
Kelly Shuster <kelly.shuster@gmail.com>
2015-05-10 14:35:52 -0600

    Friend grid view customized

--- app/src/main/java/com/kiokrofovitch/ribbit/ui/FriendsFragment.java
+++ app/src/main/java/com/kiokrofovitch/ribbit/ui/FriendsFragment.java
@@ -1,91 +1,100 @@
 package com.kiokrofovitch.ribbit.ui;
 
-import java.util.List;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
 
-import com.kiokrofovitch.ribbit.utils.ParseConstants;
 import com.kiokrofovitch.ribbit.R;
+import com.kiokrofovitch.ribbit.adapters.UserAdapter;
+import com.kiokrofovitch.ribbit.utils.ParseConstants;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
-import android.app.AlertDialog;
-import android.os.Bundle;
-import android.support.v4.app.ListFragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import java.util.List;
 
-public class FriendsFragment extends ListFragment {
-	
-	public static final String TAG = FriendsFragment.class.getSimpleName();
-	
-	protected List<ParseUser> mFriends;
-	protected ParseRelation<ParseUser> mFriendsRelation;
-	protected ParseUser mCurrentUser;
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		
-		View rootView = inflater.inflate(R.layout.fragment_friends,
-				container, false);
-		
-		return rootView;
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		
-		mCurrentUser = ParseUser.getCurrentUser();
-		mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
-		
-		getActivity().setProgressBarIndeterminateVisibility(true);
-		
-		ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
-		query.addAscendingOrder(ParseConstants.KEY_USERNAME);
-		
-		query.findInBackground(new FindCallback<ParseUser>() {
+public class FriendsFragment extends Fragment {
 
-			@Override
-			public void done(List<ParseUser> friends, ParseException e) {
-				getActivity().setProgressBarIndeterminateVisibility(false);
-				
-				if( e == null ){
-					mFriends = friends;
-	
-					String[] usernames = new String[mFriends.size()];
-					
-					int i = 0;
-					for (ParseUser user : mFriends) {
-						usernames[i] = user.getUsername();
-						i++;
-					}
-					
-					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-							getListView().getContext(), 
-							android.R.layout.simple_list_item_1, 
-							usernames);
-					
-					setListAdapter(adapter);
-				}
-				else {
-					Log.e(TAG, e.getMessage());
-					
-					AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+    public static final String TAG = FriendsFragment.class.getSimpleName();
 
-					builder.setMessage(e.getMessage())
-							.setTitle(R.string.error_title)
-							.setPositiveButton(android.R.string.ok, null);
+    protected List<ParseUser> mFriends;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
+    protected GridView mGridView;
 
-					AlertDialog dialog = builder.create();
-					dialog.show();
-				}
-			}
-		});
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_friends,
+                container, false);
+
+        mGridView = (GridView) rootView.findViewById(R.id.friends_grid);
+
+        TextView emptyTextView = (TextView) rootView.findViewById(android.R.id.empty);
+        mGridView.setEmptyView(emptyTextView);
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+
+        getActivity().setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
+        query.addAscendingOrder(ParseConstants.KEY_USERNAME);
+
+        query.findInBackground(new FindCallback<ParseUser>() {
+
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                getActivity().setProgressBarIndeterminateVisibility(false);
+
+                if (e == null) {
+                    mFriends = friends;
+
+                    String[] usernames = new String[mFriends.size()];
+
+                    int i = 0;
+                    for (ParseUser user : mFriends) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+
+                    if(mGridView.getAdapter() == null) {
+                        UserAdapter adapter = new UserAdapter(getActivity(), mFriends);
+                        mGridView.setAdapter(adapter);
+                    }
+                    else {
+                        ((UserAdapter)mGridView.getAdapter()).refill(mFriends);
+                    }
+
+                } else {
+                    Log.e(TAG, e.getMessage());
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                    builder.setMessage(e.getMessage())
+                            .setTitle(R.string.error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+    }
 }
